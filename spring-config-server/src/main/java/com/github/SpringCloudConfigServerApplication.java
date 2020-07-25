e109432d9a690ecf0355abbf0284fe7a57f19636
abhik <avik5324@gmail.com>
2020-07-26 02:13:38 +0530

    Coorecting Git Structure

--- spring-config-server/src/main/java/com/github/SpringCloudConfigServerApplication.java
+++ spring-config-server/src/main/java/com/github/SpringCloudConfigServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	public static void main(String[] arg) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, arg);
 	}
 
 }
